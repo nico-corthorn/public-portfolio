473fca78027909316ed9791f8a5c012bc1f7cded
Nico <nicolas_corthorn@berkeley.edu>
2019-12-08 19:21:49 -0500

    Price/Book and Momentum

--- dataProcessing.py
+++ dataProcessing.py
@@ -1,14 +1,16 @@
 
 import pandas as pd
 import numpy as np
-from datetime import datetime
 from concurrent import futures
+from datetime import datetime, timedelta
+from pandas.tseries.offsets import BDay
 import managerSQL
 
 
 class DataProcessing:
-    def __init__(self):
-        self.sql_manager = managerSQL.ManagerSQL()
+    def __init__(self, params):
+        self.params = params['data_processing']
+        self.sql_manager = managerSQL.ManagerSQL(params['db'])
         self.elements = self.get_elements()
         self.equity = self._get_equity()
         self.shares = self._get_shares()
@@ -19,41 +21,71 @@ class DataProcessing:
         symbols = [s for s in symbols_all if s not in symbols_fund]
         return symbols
 
-    def compute_pb(self, max_workers=6):
+    def process(self):
+        """ Main execution of DataProcessing class. """
+        if self.params['compute_factors']:
+            self.compute_loop(self.elements, self.compute_factors)
+
+    def compute_parallel(self, args, fun, max_workers=6):
+        """ General purpose parallel computing function. """
         print("\nProcessing symbols in parallel")
         ex = futures.ThreadPoolExecutor(max_workers=max_workers)
-        args = self.elements
-        ex.map(self.compute_pb_symbol, args)
+        ex.map(fun, args)
 
-    def compute_pb_loop(self, max_workers=6):
+    def compute_loop(self, args, fun):
+        """ For debugging purposes. """
         print("\nProcessing symbols one by one")
-        for symbol in self.elements:
-            self.compute_pb_symbol(symbol)
+        for symbol in args:
+            fun(symbol)
 
-    def compute_pb_symbol(self, symbol):
+    def compute_factors(self, symbol):
         """ Compute price to book value of symbol and upload to database. """
         t0 = datetime.now()
 
         df_prices = self.sql_manager.select_query("select * from prices where symbol = '" + symbol + "'")
-        df_equity = self.equity[self.equity.symbol == symbol].sort_values('ddate', ascending=False)
-        df_shares = self.shares[self.shares.symbol == symbol].sort_values('ddate', ascending=False)
-        df_prices['equity'] = np.nan
-        df_prices['shares'] = np.nan
 
-        # Equity
-        for row in df_equity.index:
-            ddate = df_equity.ddate[row]
-            equity = df_equity.equity[row]
-            df_prices.loc[(df_prices.date > ddate) & pd.isna(df_prices.equity), 'equity'] = equity
+        if df_prices.shape[0] > 0:
+            df_equity = self.equity[self.equity.symbol == symbol].sort_values('ddate', ascending=False)
+            df_shares = self.shares[self.shares.symbol == symbol].sort_values('ddate', ascending=False)
+            df_prices['equity'] = np.nan
+            df_prices['shares'] = np.nan
 
-        # Shares
-        for row in df_shares.index:
-            ddate = df_shares.ddate[row]
-            shares = df_shares.shares_basic[row]
-            df_prices.loc[(df_prices.date > ddate) & pd.isna(df_prices.shares), 'shares'] = shares
+            # Equity
+            for row in df_equity.index:
+                ddate = df_equity.ddate[row]
+                equity = df_equity.equity[row]
+                df_prices.loc[(df_prices.date > ddate) & pd.isna(df_prices.equity), 'equity'] = equity
 
-        # Upload data to db
-        self.sql_manager.upload_df('prices_fundamentals', df_prices)
+            # Shares
+            for row in df_shares.index:
+                ddate = df_shares.ddate[row]
+                shares = df_shares.shares_basic[row]
+                df_prices.loc[(df_prices.date > ddate) & pd.isna(df_prices.shares), 'shares'] = shares
+
+            # Market Price
+            # Adjust by dividends paid since last equity published...
+            df_prices['mcap'] = df_prices['close']*df_prices['shares']
+
+            # Price to Book Value
+            df_prices['pb'] = df_prices['mcap']/df_prices['equity']
+
+            # Momentum
+            df_prices_12m = df_prices.copy()
+            df_prices_1m = df_prices.copy()
+            cols = ['date', 'adjclose']
+            df_prices_12m = df_prices_12m[cols]
+            df_prices_1m = df_prices_1m[cols]
+            df_prices_12m['date'] = pd.to_datetime(df_prices_12m.date + BDay(260))
+            df_prices_1m['date'] = pd.to_datetime(df_prices_1m.date + BDay(20))
+            df_prices['date'] = pd.to_datetime(df_prices.date)
+            df_prices = df_prices.merge(df_prices_12m, left_on='date', right_on='date', how='left', suffixes=('', '_12m'))
+            df_prices = df_prices.merge(df_prices_1m, left_on='date', right_on='date', how='left', suffixes=('', '_1m'))
+            df_prices['mom'] = np.log(df_prices.adjclose_1m) - np.log(df_prices.adjclose_12m)
+            df_prices['mom'] = df_prices.mom.fillna(method='ffill', limit=5)
+            df_prices.drop(columns=['adjclose_12m', 'adjclose_1m'], inplace=True)
+
+            # Upload data to db
+            self.sql_manager.upload_df('prices_fundamentals', df_prices)
 
         t1 = datetime.now()
         print('Processing successful for {0} ({1:.2f} sec)'.format(symbol, (t1 - t0).total_seconds()))
--- main.py
+++ main.py
@@ -5,48 +5,46 @@ import dataProcessing
 
 
 def initialize_crawlers(params):
-	""" Initialization is done avoiding unnecessary processing. """
-	crawlers = {}
-	if params['scrapers']['tiingo']['activate']:
-		crawlers['tiingo'] = webScraper.TiingoScraper(params['scrapers']['tiingo'], params['db'])
-	if params['scrapers']['iex']['activate']:
-		crawlers['iex'] = webScraper.IexScraper(params['scrapers']['iex'], params['db'])
-	if params['scrapers']['sec']['activate']:
-		crawlers['sec'] = webScraper.SecScraper(params['scrapers']['sec'], params['db'])
-	return crawlers
+    """ Initialization is done avoiding unnecessary processing. """
+    crawlers = {}
+    if params['scrapers']['tiingo']['activate']:
+        crawlers['tiingo'] = webScraper.TiingoScraper(params['scrapers']['tiingo'], params['db'])
+    if params['scrapers']['iex']['activate']:
+        crawlers['iex'] = webScraper.IexScraper(params['scrapers']['iex'], params['db'])
+    if params['scrapers']['sec']['activate']:
+        crawlers['sec'] = webScraper.SecScraper(params['scrapers']['sec'], params['db'])
+    return crawlers
 
 
 def main():
+    # Config
+    with open('config.json') as json_file:
+        params = json.load(json_file)
 
-	# Config
-	with open('config.json') as json_file:
-		params = json.load(json_file)
+    # Scrapers
+    crawlers = initialize_crawlers(params)
+    for crawler in crawlers.values():
+        crawler.build()
+        crawler.compute()
 
-	# Scrapers
-	crawlers = initialize_crawlers(params)
-	for crawler in crawlers.values():
-		crawler.build()
-		crawler.compute()
+    # Data processing
+    if params['data_processing']['activate']:
+        processor = dataProcessing.DataProcessing(params)
+        processor.process()
 
-	# Data processing
-	if params['data_processing']['activate']:
-		processor = dataProcessing.DataProcessing()
-		if params['data_processing']['compute_db']:
-			processor.compute_pb()
+    # Outliers
 
-	# Outliers
+    # Regressions
 
-	# Regressions
+    # Forecast alpha
 
-	# Forecast alpha
+    # Forecast risk
 
-	# Forecast risk
-
-	# Optimizers
+    # Optimizers
 
 
 if __name__ == "__main__":
-	main()
+    main()
 
 
 
